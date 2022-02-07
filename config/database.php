c0758ca77c56128c6267fcb45212a66d9c5c0773
Võ Văn Cường <44707601+cuonghusc@users.noreply.github.com>
2022-02-07 11:12:27 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -37,7 +37,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => database_path('database.sqlite'),
+            'database' => env('DB_DATABASE',database_path('database.sqlite')),
             'prefix' => '',
         ],
 
